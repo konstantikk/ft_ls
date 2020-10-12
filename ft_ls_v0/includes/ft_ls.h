234735f46037413526d47b9aec1dfe2caa58634a
vera <vera@MacBook-Pro-vera.local>
2020-10-13 01:37:55 +0300

    Add comments and some structг

--- ft_ls_v0/includes/ft_ls.h
+++ ft_ls_v0/includes/ft_ls.h
@@ -28,10 +28,25 @@ typedef enum e_exec {
 ///also will need int for flags  or mb char will be enough
 
 ///will need macros for convenient access to the fields of structs (mb)
+typedef struct  s_dir // can be a single file
+{
+    char *dir_name; // name of main directory
+    char *output_line; // formatted string for final output in this directory
+    int *time; //time created directory or file, key for sorting, maybe it's not int
 
-typedef struct s_handler {
-	t_pvec* dir_vec;
-	t_dirent* read_ptr;
+}               t_dir;
+
+typedef struct s_handler // main struct for project
+{
+
+	t_pvec* dir_vec; // one node is struct t_dir
+	t_dirent* read_ptr; //adjacent vector, each node contains files contains in directory
+	unsigned int flag_r: 1;
+	unsigned  int flag_t: 1;
+    unsigned  int flag_recursive: 1;
+    unsigned  int flag_l: 1;
+    unsigned  int flag_a: 1;
+
 } 				t_handler;
 
 #endif //FT_LS_FT_LS_H
--- ft_ls_v0/src/main.c
+++ ft_ls_v0/src/main.c
@@ -13,8 +13,19 @@ void finish_him() {
 ///or maybe we could make it global??
 ///we will probably need to save the names of directories not the ptrs to them (!!!(fixed))
 
-void dir_push_wrapper(t_handler* handler, const char* dir) {
-	if (ft_ptr_vec_pushback(handler->dir_vec, (void*)dir) == -1)
+void dir_push_wrapper(t_handler* handler, const char* dir_name)
+/*
+ * push struct t_dir to main struct t_handler
+ *
+ */
+{
+    t_dir *dir_holder;
+
+    if ((dir_holder = (t_dir *)ft_memalloc(sizeof(t_dir))) == NULL)
+        finish_him(); // TODO free struct
+    if ((dir_holder->dir_name = ft_strdup(dir_name)) == NULL)
+        finish_him(); // TODO
+	if (ft_ptr_vec_pushback(handler->dir_vec, (void*)dir_holder) == -1)
 		///freshers
 		finish_him();
 }
@@ -23,20 +34,23 @@ void dir_push_wrapper(t_handler* handler, const char* dir) {
 ///may be takes 4 args size of dirs, dirs, and flags for execution and for the function
 
 
-t_handler* init_handler(const int size, const char** dirs, const t_exec exec) {
+t_handler* init_handler(const int size, const char** dirs, const t_exec exec)
+{
 	t_handler* handler;
 	register int i;
 
 	if ((handler = (t_handler*)ft_memalloc(sizeof(t_handler))) == NULL)
 		finish_him();
 
-	if ((handler->dir_vec = ft_ptr_vec_init()) == NULL) {
+	if ((handler->dir_vec = ft_ptr_vec_init()) == NULL)
+	{
 		ft_memdel((void**)&handler);
 		finish_him();
 	}
 	i = 1;
 	if (exec == DIRS_IN_ARGS)
-		while (i < size) {
+		while (i < size)
+		{
 			dir_push_wrapper(handler, dirs[i]);
 			++i;
 		}
@@ -50,7 +64,8 @@ t_handler* init_handler(const int size, const char** dirs, const t_exec exec) {
 ///this function needs to be well thought out
 
 ///opening and closing of dir is happening here
-void output_content(t_handler* handler, const char* dir_name, const int dirs_size) {
+void output_content(t_handler* handler, const char* dir_name, const int dirs_size)
+{
 	DIR* dir_ptr = opendir(dir_name);
 
 	if (dir_ptr == NULL)
@@ -66,18 +81,71 @@ void output_content(t_handler* handler, const char* dir_name, const int dirs_siz
 		finish_him();
 }
 
-void read_dirs(t_handler* handler) {
+void read_dirs(t_handler* handler)
+{
 	register int i;
 
 	i = 0;
 	///slightly better
-	while (i < handler->dir_vec->length) {
+	while (i < handler->dir_vec->length)
+	{
 		output_content(handler, handler->dir_vec->data[i], handler->dir_vec->length);
 		++i;
 	}
 }
 
-int main(int argc, char** argv) {
+int is_dir(char *dir_name)
+/*
+ * check directory this object or not
+ * in case of true return 1 otherwise 0
+ */
+{
+    return (1);
+}
+
+char *folder_processing(t_handler *handler, t_dir *dir) // recursive flag, t/l/
+/*
+ * read directory,
+ * local sorting file in directory, formatting output and save in dir->output_line
+ */
+{
+    char *output_line;
+
+    if (is_dir(dir->dir_name))
+        ;
+        // collect all files and dirs in folder + formatting
+        // sorting by key
+        // output_line
+    else
+        ;//output_line =  function formating str
+
+
+    return (output_line);
+}
+
+void ft_ls(t_handler *handler)
+/*
+ * main circle on directories, with recursive flag new directories added to the end
+ */
+{
+    int     i;
+
+    i = -1;
+    while (++i < (int)handler->dir_vec->length)
+    {
+        folder_processing()
+    }
+    // sort directory in dir_vec by key, check flag -r -t ...
+
+    //output cycle on vector and fill t_cvec *final_output ?
+
+    // write(final_output)
+
+    // free memory
+}
+
+int main(int argc, char** argv)
+{
 	t_handler* handler;
 
 	///here will be happening parsing of args and dirs
@@ -87,8 +155,14 @@ int main(int argc, char** argv) {
 
 	///argv will be changed for array of actual dir names
 	handler = init_handler(argc, (const char**)argv,
-						argc == 1? NO_DIRS_IN_ARGS : DIRS_IN_ARGS);
-	read_dirs(handler);
+						argc == 1 ? NO_DIRS_IN_ARGS : DIRS_IN_ARGS);
+
+	for (int i=0; i < handler->dir_vec->length; i++)
+    {
+	    printf("%s \n", ((t_dir*)(handler->dir_vec->data[i]))->dir_name);
+    }
+
+    //read_dirs(handler);
 
 	///display_content();
 	///free_data();
