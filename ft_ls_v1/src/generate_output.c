07bb643c31279f2fa5a886c4afdcb8135292efc0
vera <vera@MacBook-Pro-vera.local>
2020-11-30 01:15:48 +0300

    Fix bugs with combine output (dir, files) + -l flag

--- ft_ls_v1/src/generate_output.c
+++ ft_ls_v1/src/generate_output.c
@@ -6,31 +6,33 @@
 
 void collect_files(t_cvec *output_str, t_node *node, size_t max, int last_flag)
 {
-    void    *temp;
+    char    temp[max - node->d_name_len + 1];
 
+    ft_bzero((void *)temp,max - node->d_name_len + 1);
     ft_chr_vec_pushback(output_str, node->d_name);
-    temp = ft_strnew(max - node->d_name_len + 1); // TODO how did not use malloc? or use malloc once []
     if (!last_flag)
-        ft_chr_vec_pushback(output_str,(char *)ft_memset(temp, ' ',
+        ft_chr_vec_pushback(output_str,(char *)ft_memset((void *)temp, ' ',
                                               max - node->d_name_len + 1));
     else
         ft_chr_vec_pushback(output_str, "\n");
-    ft_memdel(&temp);
 }
 
 void add_to_output(t_cvec *output_str, t_pvec *vec, int *max_list)
 {
     int     i;
-    void    *temp;
+    char    temp[16];
+    t_cvec  *elem;
 
     i = -1;
-    temp = ft_strnew(16); // TODO no malloc []
-    while (++i < vec->length)
+    ft_bzero((void *)temp, 16);
+    while (vec->length > 0)
     {
+        i++;
+        elem = (t_cvec *)ft_ptr_vec_popfront(vec);
         if (i % 8 == 2  || i % 8 == 3 || i % 8 == 4 || i % 8 == 5)
         {
-            ft_chr_vec_pushback(output_str, (char *)ft_memset(temp, ' ',
-                                                              max_list[i % 8] - ((t_cvec *)(vec->data)[i])->length + 2));
+            ft_chr_vec_pushback(output_str, (char *)ft_memset((void *)temp, ' ',
+                                                              max_list[i % 8] - elem->length + 2));
             ft_bzero(temp, 16);
         }
         else if (i % 8 == 6 || i % 8 == 7)
@@ -38,7 +40,7 @@ void add_to_output(t_cvec *output_str, t_pvec *vec, int *max_list)
         else
             ;
         if (i % 8 != 0)
-            ft_chr_vec_pushback(output_str, ((t_cvec *)(vec->data)[i])->data);
+            ft_chr_vec_pushback(output_str, elem->data);
         if (i % 8 == 0 && i != 0)
             ft_chr_vec_pushback(output_str,"\n");
     }
@@ -57,6 +59,14 @@ void output_manager(t_handler *handler, t_pvec *processed_nodes)
     while (++i < processed_nodes->length)
     {
         cond = NODE(processed_nodes, i)->nodes == NULL;
+        if (i > 1 && !cond && NODE(processed_nodes, i - 1)->nodes == NULL
+        && (handler->flags & LIST))
+        {
+            add_to_output(handler->output_str, handler->output_vec_util, handler->output_max_list_util);
+            ft_chr_vec_pushback(handler->output_str,"\n");
+
+        }
+
         if (processed_nodes->length == 1 && !cond &&
             NODE(processed_nodes, i)->nodes->length == 0) // TODO check empty dir
             continue;
@@ -65,7 +75,8 @@ void output_manager(t_handler *handler, t_pvec *processed_nodes)
         else
             files_processing(NODE(processed_nodes, i), handler, processed_nodes->length, i);
     }
+    if ((handler->flags & LIST) && handler->output_str->length == 0)
+        add_to_output(handler->output_str, handler->output_vec_util, handler->output_max_list_util); // add new line
     write(1, handler->output_str->data, handler->output_str->length);
     //TODO FREE all
-    //ft_printf("%s", output_str->data);
 }
--- ft_ls_v1/src/processing_file_output.c
+++ ft_ls_v1/src/processing_file_output.c
@@ -5,27 +5,25 @@
 
 void files_processing(t_node *node, t_handler *handler, size_t p_len, size_t i)
 {
-    t_cvec *out;
-
-    out = handler->output_str;
     if (handler->flags & LIST)
     {
         collect_inf(node, handler->output_vec_util, handler->output_max_list_util);
-        add_to_output(out, handler->output_vec_util, handler->output_max_list_util);
+       // add_to_output(handler->output_str, handler->output_vec_util, handler->output_max_list_util);
         return;
     }
     if (p_len != 1)
     {
-        collect_files(out, node, find_max(handler->processed_nodes),
-                     (i <= p_len - 2 &&
-                     (NODE(handler->processed_nodes, i + 1)->nodes != NULL))
-                     || i == p_len - 1);
+        collect_files(handler->output_str, node, find_max(handler->processed_nodes),
+                      (i <= p_len - 2 &&
+                       (NODE(handler->processed_nodes, i + 1)->nodes != NULL))
+                      || i == p_len - 1);
         if (i <= p_len - 2 && (NODE(handler->processed_nodes, i + 1)->nodes != NULL))
-            ft_chr_vec_pushback(out, "\n");
+            ft_chr_vec_pushback(handler->output_str, "\n");
     }
     else
     {
-        ft_chr_vec_pushback(out, node->d_name);
-        ft_chr_vec_pushback(out, "\n");
+        ft_chr_vec_pushback(handler->output_str, node->d_name);
+        ft_chr_vec_pushback(handler->output_str, "\n");
     }
+
 }
